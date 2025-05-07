8d01b453e7555953ca13aa4613fe0873073dba90
samuelromeu <samuelromeu20063@gmail.com>
2025-05-06 21:51:13 -0300

    retirando anotações

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 function mostrar(tag) {
-    // Esconde todas as páginas
     document.querySelectorAll('page-1, page-2, page-3').forEach(el => {
       el.classList.remove('ativa');
     });
     document.querySelector(tag).classList.add('ativa');
 
-    // Atualiza o item ativo da navbar
     document.querySelectorAll('.nav-link').forEach(link => {
       link.classList.remove('active');
     });
